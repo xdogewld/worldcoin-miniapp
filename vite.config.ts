c200ab3ba53ade5b4e6e2b5c9b97ae3619a1b094
xdogewld <tukulrenaldi28@gmail.com>
2025-04-14 06:32:52 +0700

    fix: remove server config for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: false, // pastikan ini false atau hapus property 'https'
-    port: 5173,
-  },
 });
